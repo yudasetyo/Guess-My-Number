cd417ef8b7579fd9d4c47911e52baf47a3ac192d
yudasetyo <yudasetyo07@gmail.com>
2023-12-01 18:44:52 +0700

    feat: manipulating CSS styles

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // No Input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number inserted!';
+
+    // Player Wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number 🎉!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // Number too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Number to high';
@@ -20,6 +27,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'Game Over!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // Number too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Number to low';
