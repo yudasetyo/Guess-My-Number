997f5c24be0226005761b15fe6edc460ea20483c
yudasetyo <yudasetyo07@gmail.com>
2023-12-01 18:28:46 +0700

    feat: implementing game logic

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 30) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number inserted!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 🎉!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Number to high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Number to low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
